74a2726e2b3300580f495bab79f642773029f3cc
kzacristo <profissionalchrisostomo@gmail.com>
2018-02-17 15:36:21 -0200

    teste com o monitor lcd

--- medidoArduino/oximetria.ino
+++ medidoArduino/oximetria.ino
@@ -1,6 +1,15 @@
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
 
+// pin 8 - Serial clock out (SCLK)
+// pin 9 - Serial data out (DIN)
+// pin 10 - Data/Command select (D/C)
+// pin 11 - LCD chip select (CS/CE)
+// pin 12 - LCD reset (RST)
+
+Adafruit_PCD8544 lcd = Adafruit_PCD8544(8, 9, 10, 11, 12);
 #define REPORTING_PERIOD_MS   1000
 
 PulseOximeter spo;
@@ -12,6 +21,8 @@ void onBeatDetected(){
 }
 
 void setup(){
+    Serial.begin(9600);
+
     Serial.begin(115200);
 
     Serial.print("Inicializando oximetro de pulso ...");
@@ -36,11 +47,11 @@ void spoUpdate(){
 
     if(millis() - tslastReport > REPORTING_PERIOD_MS){
        Serial.print("Heart rate:");
-        Serial.print(pox.getHeartRate());
+        Serial.print(spo.getHeartRate());
         Serial.print("bpm / SpO2:");
-        Serial.print(pox.getSpO2());
+        Serial.print(spo.getSpO2());
         Serial.print("% / temp:");
-        Serial.print(pox.getTemperature());
+        Serial.print(spo.getTemperature());
         Serial.println("C");
 
         tsLastReport = millis();
@@ -48,7 +59,17 @@ void spoUpdate(){
     }
 
 void lcd(){
-    
+    lcd.begin();
+    lcd.setContrast(60); //ajusatr o contraste
+    lcd.clearDisplay(); //apaga o buffer
+    lcd.setTextSize(2); //seta o tamanho da fonte
+    lcd.setTextColor(BLACK); //seta a cor da fonte
+    lcd.setCursor(0,0); //seta a posiçãod o setCursor
+
+    lcd.println("bpm / SpO2");
+    lcd.println(" % ");
+
+    delay(2000);
 }
 
 }
